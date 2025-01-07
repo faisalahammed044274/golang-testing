8fb74b0b9d0fff2eab55ebe9aac318cd7d21e6ee
faisalahammed044274 <faisalahammed044274@gmail.com>
2025-01-07 12:29:50 +0600

    if else if else

--- 03.if-else-switch.go
+++ 03.if-else-switch.go
@@ -3,13 +3,29 @@ package main
 import "fmt"
 
 func main() {
-age := 19;
+age := 20;
+sex :="male"
+sex = "female"
 
-if age >= 18 {
-	fmt.Println("You are an adult....");
-}else if(age <= 10){
-	fmt.Println("You are a child....");
-} else {
-	fmt.Println("You are a teenager....");
+if age == 20 && sex == "male" {
+	fmt.Println("You are a male of 20 years");
+}elseif(age == 60 && sex == "male"){
+	fmt.Println("You are a male of 60 years");
+}elseif(age==20 && sex=="female"){
+	fmt.Println("You are a female of 20 years");
+	}else{
+		fmt.Println("You are not a male of 20 years");
+		}
+// if age >= 18 {
+// 	fmt.Println("You are an adult....");
+// }else if(age <= 10){
+// 	fmt.Println("You are a child....");
+// } else {
+// 	fmt.Println("You are a teenager....");
+// }
 }
-}
+
+//>, >=, <, <=, ==, !=, !=0, &&, ||, !, switch, for
+// and => &&
+// or => ||
+// not => !
